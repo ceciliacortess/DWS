6cad3ed46b88143c750a3f79807c285088556fb1
ceciliacortess <cecorvel2004@gmail.com>
2023-11-22 18:20:03 +0100

    últimos cambios ejercicio 9 y 10

--- UD3 Envío de datos y archivos al servidor/ejercicio 9 y 10/ejercicio9.php
+++ UD3 Envío de datos y archivos al servidor/ejercicio 9 y 10/ejercicio9.php
@@ -1,59 +1,58 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $nombre = isset($_POST['nombre']) ? $_POST['nombre'] : '';
-    $email = isset($_POST['email']) ? $_POST['email'] : '';
-    $usuario = isset($_POST['usuario']) ? $_POST['usuario'] : '';
-    $password = isset($_POST['pasword']) ? $_POST['pasword'] : '';
-    $confpassword = isset($_POST['confpasword']) ? $_POST['confpasword'] : '';
-    $bloqueado = isset($_POST['bloqueado']) ? $_POST['bloqueado'] : '';
+    // Validar y obtener los valores de los campos
+    $nombre = isset($_POST['nombre']) ? trim($_POST['nombre']) : '';
+    $email = isset($_POST['email']) ? trim($_POST['email']) : '';
+    $usuario = isset($_POST['usuario']) ? trim($_POST['usuario']) : '';
+    $password = isset($_POST['password']) ? trim($_POST['password']) : '';
+    $confpassword = isset($_POST['confpassword']) ? trim($_POST['confpassword']) : '';
+    $bloqueado = isset($_POST['bloqueado']) ? trim($_POST['bloqueado']) : '';
     $departamento = isset($_POST['departamento']) ? $_POST['departamento'] : [];
 
-    // Comprobar si los campos están vacíos
-    if (empty($nombre) || empty($email) || empty($usuario) || empty($password) || empty($confpassword) || empty($bloqueado) || empty($departamento)) {
-        return '<p>Uno o más campos están vacíos. Por favor, completa todos los campos.</p>';
-    } else {
-        // Construir y devolver los resultados
-        echo '<p><strong>Nombre y apellidos:</strong> ' . htmlspecialchars($nombre) . '</p>';
-        echo '<p><strong>Email:</strong> ' . htmlspecialchars($email) . '</p>';
-        echo '<p><strong>Nombre de usuario:</strong> ' . htmlspecialchars($usuario) . '</p>';
-        echo '<p><strong>Contraseña:</strong> ' . htmlspecialchars($password) . '</p>';
-        echo '<p><strong>Confirmar contraseña:</strong> ' . htmlspecialchars($confpassword) . '</p>';
-        echo '<p><strong>Bloqueado:</strong> ' . htmlspecialchars($bloqueado) . '</p>';
-        echo '<p><strong>Departamento:</strong> ' . implode(', ', $departamento) . '</p>';
+    // Validar si los campos están vacíos
+    if (empty($nombre)) {
+        echo '<p>ERROR: Debe introducir un nombre válido.</p>';
+    }else if(empty($email)){
+        echo '<p>ERROR: Debe introducir un email válido.</p>';
+    }else if(empty($usuario)){
+        echo '<p>ERROR: Debe introducir un usuario válido.</p>';
+    }else if(empty($password)){
+        echo '<p>ERROR: Debe introducir una contraseña válida.</p>';
+    }else if(empty($confpassword)){
+        echo '<p>ERROR: Debe introducir una confirmación de contraseña válida.</p>';
+    }else if(empty($bloqueado)){
+        echo '<p>ERROR: Debe escoger si está bloqueado o no.</p>';
+    }else if(empty($departamento)){
+        echo '<p>ERROR: Debe escoger mínimo un departamento.</p>';
+    }else {
+        // Validar el formato del email
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            echo '<p>ERROR: La dirección de correo electrónico no es válida.</p>';
+        } else {
+            // Validar la contraseña y la confirmación de la contraseña
+            if (strlen($password) < 8) {
+                echo '<p>ERROR: La contraseña debe tener al menos 8 caracteres.</p>';
+            } elseif ($password != $confpassword) {
+                echo '<p>ERROR: Las contraseñas no coinciden. Por favor, inténtalo de nuevo.</p>';
+            } else {
+                // Construir y devolver los resultados
+                echo '<p><strong>Nombre y apellidos:</strong> ' . htmlspecialchars($nombre) . '</p>';
+                echo '<p><strong>Email:</strong> ' . htmlspecialchars($email) . '</p>';
+                echo '<p><strong>Nombre de usuario:</strong> ' . htmlspecialchars($usuario) . '</p>';
+                echo '<p><strong>Contraseña:</strong> ' . htmlspecialchars($password) . '</p>';
+                echo '<p><strong>Confirmar contraseña:</strong> ' . htmlspecialchars($confpassword) . '</p>';
+                echo '<p><strong>Bloqueado:</strong> ' . htmlspecialchars($bloqueado) . '</p>';
+                if (!empty($departamento)) {
+                    if (is_array($departamento)) {
+                        echo "<p><strong>Departamento:</strong> " . implode(", ", $departamento) . "</p>";
+                    } else {
+                        echo "<p><strong>Departamento:</strong> $departamento</p>";
+                    }
+                } else {
+                    echo "<p><strong>Departamento:</strong> Sin departamento</p>";
+                }
+            }
+        }
     }
 }
-
-if (isset($_POST['subir'])) {
-    //Recogemos el archivo enviado por el formulario
-    $archivo = $_FILES['archivo']['name'];
-    //Si el archivo contiene algo y es diferente de vacio
-    if (isset($archivo) && $archivo != "") {
-       //Obtenemos algunos datos necesarios sobre el archivo
-       $tipo = $_FILES['archivo']['type'];
-       $tamano = $_FILES['archivo']['size'];
-       $temp = $_FILES['archivo']['tmp_name'];
-       //Se comprueba si el archivo a cargar es correcto observando su extensión y tamaño
-      if (!((strpos($tipo, "gif") || strpos($tipo, "jpeg") || strpos($tipo, "jpg") || strpos($tipo, "png")) && ($tamano < 2000000))) {
-         echo '<div><b>Error. La extensión o el tamaño de los archivos no es correcta.<br/>
-         - Se permiten archivos .gif, .jpg, .png. y de 200 kb como máximo.</b></div>';
-      }
-      else {
-         //Si la imagen es correcta en tamaño y tipo
-         //Se intenta subir al servidor
-         if (move_uploaded_file($temp, 'images/'.$archivo)) {
-             //Cambiamos los permisos del archivo a 777 para poder modificarlo posteriormente
-             chmod('images/'.$archivo, 0777);
-             //Mostramos el mensaje de que se ha subido co éxito
-             echo '<div><b>Se ha subido correctamente la imagen.</b></div>';
-             //Mostramos la imagen subida
-             echo '<p><img src="images/'.$archivo.'"></p>';
-         }
-         else {
-            //Si no se ha podido subir la imagen, mostramos un mensaje de error
-            echo '<div><b>Ocurrió algún error al subir el fichero. No pudo guardarse.</b></div>';
-         }
-       }
-    }
- }
-
 ?>
