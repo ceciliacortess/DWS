4d65b017d66670628af365d7e7cd5608adff41c4
ceciliacortess <cecorvel2004@gmail.com>
2024-02-24 13:05:48 +0100

    terminar 7.2

---
+++ LARAVEL/pruebaAuth/resources/views/dashboard.blade.php
@@ -0,0 +1,23 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            {{ __("You're logged in!") }}
+            <br><br>
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    @if (Auth::check())
+                    <a href="{{ route('protegido') }}">Ir a la página protegida</a>
+                    @endif
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</x-app-layout>
